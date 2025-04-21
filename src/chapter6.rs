5616407d99276a260a12a5af3069bced5fd1c2c3
Tim <tim.kochs@hotmail.com>
2025-04-21 20:49:00 +0200

    chapter 6

--- src/chapter6.rs
+++ src/chapter6.rs
@@ -5,6 +5,7 @@
 
 pub fn run() {
     create();
+    with_data();
 }
 
 enum MyEnum {
@@ -16,6 +17,14 @@ enum MyEnum {
 enum MyEnumButBetter {
     A(u32),
     B(i32),
+    C { x: i32, y: i32 }, // this version contains an "anonymous struct"
+    D(f32, f32),          // this version contains an tuple
+}
+
+impl MyEnumButBetter {
+    fn call(&self) {
+        // method on enum
+    }
 }
 
 fn create() {
@@ -28,6 +37,32 @@ fn create() {
 fn with_data() {
     let a = MyEnumButBetter::A(30);
     let b = MyEnumButBetter::B(-30);
+    let c = MyEnumButBetter::C { x: 20, y: 20 };
+    let d = MyEnumButBetter::D(1.0, 1.0);
+    a.call();
+    b.call();
+    c.call();
+    d.call();
+    // accessing the inner value using match
+
+    match a {
+        MyEnumButBetter::A(x) => println!("{x}"),
+        _ => {
+            println!("Not a valid entry!");
+        }
+    }
+    // if let statement kinda works like a match but can be used if match is to verbose
+    if let MyEnumButBetter::B(-30) = b {
+        println!("Minus thirty!");
+    }
+    if let MyEnumButBetter::D(a, b) = d {
+        println!("x:{a}, y:{b}");
+    }
+    if let MyEnumButBetter::D(a, b) = c {
+        println!("x:{a}, y:{b}");
+    } else {
+        println!("Wrong pattern!");
+    }
 }
 
 fn test(_: MyEnum) {}
