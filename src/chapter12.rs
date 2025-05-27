5342ecfb7c03308d3ea5d7cc0713c04fe483ceb8
Tim Kochs <tkochs@techfak.uni-bielefeld.de>
2025-05-27 12:15:33 +0200

    chapter12

--- src/chapter12.rs
+++ src/chapter12.rs
@@ -1,7 +1,32 @@
+/*
+* 'The organizational problem of allocating responsibility for multiple tasks to
+* the main function is common to many binary projects. As a result, the Rust
+* community has developed a process to use as a guideline for splitting the
+* separate concerns of a binary program when main starts getting large. The
+* process has the following steps:
+*
+* • Split your program into a main.rs and a lib.rs and move your program’s logic to lib.rs.
+* • As long as your command line parsing logic is small, it can remain in main.rs.
+* • When the command line parsing logic starts getting complicated, extract it from main.rs and move it to lib.rs.
+*
+* The responsibilities that remain in the main function after this process
+* should be limited to the following:
+*
+* • Setting up any other configuration
+* • Calling the command line parsing logic with the argument values
+* • Handling the error if run returns an error
+* • Calling a run function in lib.rs'
+* [direct quote from the book]
+*/
+
+// since we moved the code to lib.rs we need to import it again
+use rust_book::{execute, Config};
+
 pub fn run() {
     println!("Chapter12: Building a CLI application");
     println!("This is a recap of all the things done in previous chapters..");
     println!("I highly suggest working throught this chapter on your own.");
+    println!("*************************************************\n");
     mini_main();
 }
 
@@ -9,5 +34,12 @@ pub fn run() {
 // like 'cargo run searchstring file.txt'
 fn mini_main() {
     let args: Vec<String> = std::env::args().collect();
-    println!("{:?}", args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+    if let Err(e) = execute(config) {
+        eprintln!("Application error {}", e);
+        std::process::exit(1);
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,92 @@
 pub fn add_two(x: i32) -> i32 {
     x + 2
 }
+
+// return unit value or some
+// object that implement the error trait
+pub fn execute(config: Config) -> Result<(), Box<dyn std::error::Error>> {
+    let contents = std::fs::read_to_string(config.filename)?; // since we return a
+                                                              // Result we move the error just up
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+    for line in results {
+        println!("{}", line);
+    }
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+    case_sensitive: bool,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enought arguments!");
+        }
+        // println!("{:?}", args); // first argument is the path of the binary
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        let case_sensitive = std::env::var("CASE_INSENSITVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Pick three.
+            Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+            Rust:
+            safe, fast, productive.
+            Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
